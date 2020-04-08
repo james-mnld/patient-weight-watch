81592ae43394174f93f627499415583d5e9d0815
James Manalad <james.manalad@mail.mcgill.ca>
2020-04-08 04:56:01 -0400

    database query + live time update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from datetime import date, datetime
 import psycopg2
+import psycopg2.extras
 
 DATABASE = "weight_watcher"
 USER = "weight_watcher"
@@ -11,11 +13,42 @@ app = Flask(__name__, template_folder='templates')
 app.config["DEBUG"] = True
 
 
-@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
-# Function called upon submission of symptom webform
+
+@app.route('/dashboard')
+def dashboard():
+    command_query_db = """SELECT * FROM PATIENT_INFO"""
+    response = exec_db_command(command_query_db, 1)  # 1 returns fetchall
+    rows = build_weights(response)
+    return render_template('test.html', tabData=rows,
+                           lastUpdate=datetime.now())
+    # return render_template('dashboard.html')
+
+
+# Helper function to extract and format weight info from rec_weights column
+def build_weights(p_list):
+    new_list = []
+    for dictionary in p_list:
+        p_dict = dictionary.copy()
+        weights = p_dict["rec_weights"].split(", ")
+
+        last_weight = float(weights[-2])
+        current_weight = float(weights[-1])
+        change_weight = current_weight - last_weight
+
+        p_dict["last_weight"] = '%.2f' % last_weight
+        p_dict["current_weight"] = '%.2f' % current_weight
+        p_dict["change_weight"] = '%.2f' % change_weight
+
+        print(p_dict)
+        new_list.append(p_dict)
+    return new_list
+
+
+# Function called upon submission of symptom webform (db for database)
 @app.route('/update_db', methods=['POST'])
 def update_db():
     p_id = int(request.form["p_id"])
@@ -32,14 +65,15 @@ def update_db():
                             WHERE PATIENTID = %i \
                             """ % (p_nausea, p_skinirr, p_diffswall,
                                    p_diffbreath, p_id)
-    exec_db_command(command_update_symp)
+    exec_db_command(command_update_symp, 0)
 
-    # update rec_weights on PATIENT_INFO
+    # update rec_weights and last_submit on PATIENT_INFO
     command_update_info = """UPDATE PATIENT_INFO \
-                            SET REC_WEIGHTS = REC_WEIGHTS || '%s' \
+                            SET REC_WEIGHTS = REC_WEIGHTS || '%s', \
+                            LAST_SUBMIT = '%s' \
                             WHERE PATIENTID = %i \
-                            """ % (', '+str(p_weight), p_id)
-    exec_db_command(command_update_info)
+                            """ % (', '+str(p_weight), date.today(), p_id)
+    exec_db_command(command_update_info, 0)
 
     return render_template("submitted.html", p_weight=p_weight,
                            p_nausea=p_nausea, p_skinirr=p_skinirr,
@@ -47,12 +81,24 @@ def update_db():
 
 
 # Helper function that executes given MySQL string command
-def exec_db_command(command):
+def exec_db_command(command, withval):
     con = psycopg2.connect(database=DATABASE, user=USER, password=PASSWORD,
                            host=HOST, port=PORT)
-    cur = con.cursor()
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(command)
     con.commit()
+    if withval:  # if withval has value, return fetchall data
+        return cur.fetchall()
 
 
-app.run()
+@app.route('/update_dash')
+def update_dash():
+    command_query_db = """SELECT * FROM PATIENT_INFO"""
+    response = exec_db_command(command_query_db, 1)  # 1 returns fetchall
+    rows = build_weights(response)
+    return jsonify(time=datetime.now(), sort="DEFAULT", tab_dat=rows)
+    # return str(datetime.now())
+
+
+if __name__ == '__main__':
+    app.run()
